1a746ca4013f4e00fe1ff5ee649ac82b0b5ff52b
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-06-24 11:59:10 -0600

    added export to actions and, main and reducer

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,5 +1,25 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+
+import { useReducer } from 'react'
+export const ADD_BOOK = 'ADD_BOOK'
+export const REMOVE_BOOK = 'REMOVE_BOOK'
+export const SEARCH_BOOKS = 'SEARCH_BOOKS'
+
+
+export default function reducer(prevState, {action, payload}) {
+    switch(action) {
+    case ADD_BOOK:
+      saveToLocalStorage([...prevState.favoriteBooks, payload]);
+      return {...prevState, favoriteBooks:[...prevState.favoriteBooks, payload] }
+    case SEARCH_BOOKS:
+      return {...prevState, bookSearchResults: payload }
+    case REMOVE_BOOK:
+      saveToLocalStorage(prevState.favoriteBooks.filter(({id}) => id !== payload));
+      return {...prevState, favoriteBooks:prevState.favoriteBooks.filter(({id}) => id !== payload)}
+    default:
+      return prevState 
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
